e3e08757a598083b98cf47b9a0030f435dea895b
Devaraj <nsdevaraj@gmail.com>
2025-02-15 20:03:14 +0530

    json

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
+import react from '@vitejs/plugin-react'; 
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,5 +18,13 @@ export default defineConfig({
   ],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        tracks: './tracks.json'
+      }
+    }
   }
 });
